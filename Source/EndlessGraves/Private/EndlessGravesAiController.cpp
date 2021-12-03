8dc4d953f9a79eac4e059411a30587fe898d81e7
uoubyy <uoubyy@gmail.com>
2021-12-03 14:31:19 -0700

    spawn skeleton

--- Source/EndlessGraves/Private/EndlessGravesAiController.cpp
+++ Source/EndlessGraves/Private/EndlessGravesAiController.cpp
@@ -7,17 +7,10 @@
 void AEndlessGravesAIController::ChasingPlayer(FVector Location)
 {
 	TargetLocation = Location;
-	MoveToLocation(TargetLocation, 50.0f, false);
+	MoveToLocation(TargetLocation, 50.0f, false, true, true);
 }
 
 void AEndlessGravesAIController::RandomMovement(FVector Origin)
 {
-	//UWorld* const World = GetWorld();
-	//if (World)
-	//{
-	//	UNavigationSystemV1* NavSys = UNavigationSystemV1::GetCurrent(GetWorld());
 
-	//	if (NavSys != nullptr && NavSys->K2_GetRandomPointInNavigableRadius(World, Origin, TargetLocation, 500.0f))
-	//		MoveToLocation(TargetLocation, 50.0f);
-	//}
 }
